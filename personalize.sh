0e338789b3cb66eb0e4186bbf7340706ff36e793
Hidde Beydals <hiddeco@users.noreply.github.com>
2024-08-28 22:32:17 +0200

    Tweak `personalize.sh` to allow name config (#2)
    Signed-off-by: Hidde Beydals <hidde@hhh.computer>

--- personalize.sh
+++ personalize.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
 if [ -z "$1" ]; then
-  echo "./personalize.sh <username>"
+  echo "./personalize.sh <username> [repository_name] [image_name]"
   exit 1
 fi
 
-find . -type f -name '*.yaml' -exec sed -E -i '' s#https://github.com/[-_a-zA-Z0-9]+#https://github.com/${1}#g {} +
-find . -type f -name '*.yaml' -exec sed -E -i '' s#ghcr.io/[-_a-zA-Z0-9]+#ghcr.io/${1}#g {} +
+username=$1
+repository_name=$2
+image_name=$3
+
+find . -type f -name '*.yaml' -exec sed -E -i '' s#https://github.com/[-_a-zA-Z0-9]+#https://github.com/${username}#g {} +
+find . -type f -name '*.yaml' -exec sed -E -i '' s#ghcr.io/[-_a-zA-Z0-9]+#ghcr.io/${username}#g {} +
+
+if [ ! -z "$repository_name" ]; then
+  find . -type f -name '*.yaml' -exec sed -E -i '' "s#https://github.com/${username}/[-_a-zA-Z0-9]+#https://github.com/${username}/${repository_name}#g" {} +
+fi
+
+if [ ! -z "$image_name" ]; then
+  find . -type f -name '*.yaml' -exec sed -E -i '' "s#ghcr.io/${username}/[-_a-zA-Z0-9]+#ghcr.io/${username}/${image_name}#g" {} +
+fi
