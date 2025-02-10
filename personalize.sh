32b35f58f6df60c01d1247037a169a57613fd466
Nitish Kumar <justnitish06@gmail.com>
2025-02-10 16:36:43 +0530

    chore: update the script to ensure compatibility with OS

--- personalize.sh
+++ personalize.sh
@@ -9,13 +9,20 @@ username=$1
 repository_name=$2
 image_name=$3
 
-find . -type f -name '*.yaml' -exec sed -E -i '' s#https://github.com/[-_a-zA-Z0-9]+#https://github.com/${username}#g {} +
-find . -type f -name '*.yaml' -exec sed -E -i '' s#ghcr.io/[-_a-zA-Z0-9]+#ghcr.io/${username}#g {} +
+# Detect OS and set sed flag accordingly
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  SED_OPTS="-E -i ''"
+else
+  SED_OPTS="-E -i"
+fi
+
+find . -type f -name '*.yaml' -exec sed $SED_OPTS s#https://github.com/[-_a-zA-Z0-9]+#https://github.com/${username}#g {} +
+find . -type f -name '*.yaml' -exec sed $SED_OPTS s#ghcr.io/[-_a-zA-Z0-9]+#ghcr.io/${username}#g {} +
 
 if [ ! -z "$repository_name" ]; then
-  find . -type f -name '*.yaml' -exec sed -E -i '' "s#https://github.com/${username}/[-_a-zA-Z0-9]+#https://github.com/${username}/${repository_name}#g" {} +
+  find . -type f -name '*.yaml' -exec sed $SED_OPTS "s#https://github.com/${username}/[-_a-zA-Z0-9]+#https://github.com/${username}/${repository_name}#g" {} +
 fi
 
 if [ ! -z "$image_name" ]; then
-  find . -type f -name '*.yaml' -exec sed -E -i '' "s#ghcr.io/${username}/[-_a-zA-Z0-9]+#ghcr.io/${username}/${image_name}#g" {} +
+  find . -type f -name '*.yaml' -exec sed $SED_OPTS "s#ghcr.io/${username}/[-_a-zA-Z0-9]+#ghcr.io/${username}/${image_name}#g" {} +
 fi
